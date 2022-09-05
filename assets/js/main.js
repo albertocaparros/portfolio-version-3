e336fff30a24749a491d4290f7a23e3172a23654
Alberto Caparrós Trigo <acaparros87@gmail.com>
2022-09-05 16:09:25 +0200

    footer with active section and scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,39 @@ function toggleQualifications() {
 qualificationsHeader.forEach((element) => {
   element.addEventListener('click', toggleQualifications);
 });
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+function scrollHeader() {
+  const nav = document.getElementById('header');
+
+  if (this.scrollY >= 200) {
+    nav.classList.add('scroll-header');
+  } else {
+    nav.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
