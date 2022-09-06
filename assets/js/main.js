418d8a7ffc9c5a0a74b70fca942a4b939db02aec
Alberto Caparrós Trigo <acaparros87@gmail.com>
2022-09-06 06:28:21 +0200

    Dark theme. Scroll bar. Color improvement.

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,34 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+
+const toggleTheme = () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+};
+
+themeButton.addEventListener('click', toggleTheme);
