56fe640a2a578196ddb378b2b302e695c1423079
Alberto Caparrós Trigo <acaparros87@gmail.com>
2022-09-09 08:37:49 +0200

    Scroll up functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,16 @@ const toggleTheme = () => {
 themeButton.addEventListener('click', toggleTheme);
 
 //--------------------------------------------------------------------------------//
+
+var scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+
+  if (this.scrollY >= 560) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
+};
+
+window.addEventListener('scroll', scrollUp);
+//--------------------------------------------------------------------------------//
